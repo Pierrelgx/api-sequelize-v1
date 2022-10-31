9c56011fdedea0bca63ff8b86cb0b31da41703e7
Pierre LGX <97983718+Pierrelgx@users.noreply.github.com>
2022-10-31 16:30:42 +0100

    Update order.js

--- models/order.js
+++ models/order.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Order.init({
+    user: Datatypes.INTEGER,
     product: Datatypes.INTEGER,
     amont: DataTypes.DECIMAL,
     quantity: DataTypes.INTEGER,
